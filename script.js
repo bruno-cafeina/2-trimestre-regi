e4eb652656c76306375229114cefd459f04220be
bruno-cafeina <135143536+bruno-cafeina@users.noreply.github.com>
2024-05-24 10:48:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector("caixa-perguntas");
+const caixaAlternatvas = document.querySelector("caixa-alternaiva");
+const caixaResultado = document.querySelector
